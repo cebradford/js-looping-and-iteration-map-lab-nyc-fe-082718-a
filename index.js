bcc7674f61cdc21d28aed16ddf97be06115b87a2
casey bradford <casey.bradford@wework.com>
2018-10-17 01:13:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+
+const driverNames = ["Joe", "Bob", "Cricky"]
+
+function lowerCaseDrivers (arr){
+  for (const i=0; i>arr.length; i++)
+  return arr[i].toLowerCase;
+}
+
+lowerCaseDrivers.map(driverNames);
+
+
+function nameToAttribute(arr){
+  
+  
+}
